ab78d7d53cf587040c3d9636e126816ec2bb2dd5
AMRMUSTUNI <200044236@MUST.EDU.EG>
2024-12-07 11:21:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const welcomeText = document.getElementById('welcome-text');
+    welcomeText.style.animationPlayState = 'running'; // Starts the animation
+});
